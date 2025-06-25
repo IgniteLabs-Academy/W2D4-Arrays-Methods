f9c928cd2739e63073c1f7fcbbd6b8dba36637e1
IgniteLabs-Academy <vvelisoju@gmail.com>
2025-06-26 04:00:17 +0530

    files updated

--- test/test-2.test.js
+++ test/test-2.test.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 test('Array declaration', () => {
   const code = fs.readFileSync(path.resolve(__dirname, '../script.js'), 'utf8');
-  // This test checks: Array declaration
-  expect(code).toMatch(/let\s+fruits\s*=\s*\[.*\]/);
+  // This test checks: Array declaration (let, const, or var)
+  expect(code).toMatch(/(let|const|var)\s+fruits\s*=\s*\[.*\]/);
 });
